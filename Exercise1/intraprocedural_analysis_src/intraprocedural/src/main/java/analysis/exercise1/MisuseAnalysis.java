c8369fa5eb7fba18a53adad2a9af9655726c288a
Matthew Tam <matthewethan.tam@gmail.com>
2022-10-07 17:50:17 +0200

    adding

--- Exercise1/intraprocedural_analysis_src/intraprocedural/src/main/java/analysis/exercise1/MisuseAnalysis.java
+++ Exercise1/intraprocedural_analysis_src/intraprocedural/src/main/java/analysis/exercise1/MisuseAnalysis.java
@@ -14,17 +14,20 @@ public class MisuseAnalysis extends AbstractAnalysis{
 	protected void flowThrough(Unit unit) {
 		String invClassName = "javax.crypto.Cipher";
 		String invMethodName = "getInstance";
-		Stmt st = (Stmt) unit; // cast to statement
+		String correctConvention = "AES/GCM/PKCS5Padding";
+		Stmt st = (Stmt) unit;
 		if (st.containsInvokeExpr()) { // check if the statement contains an invoke expression
-			InvokeExpr invExpr = st.getInvokeExpr(); // retrieves the invoke expression
-			SootMethod invMeth = invExpr.getMethod();
-			SootClass invDeclClass = invExpr.getMethod().getDeclaringClass();
-			if (invMeth.getName().equals(invMethodName) && invDeclClass.getName().equals(invClassName)) {
-				Value arg = invExpr.getArg(0);
-				if (arg instanceof StringConstant) {
-					StringConstant constant = (StringConstant) arg;
-					if (!constant.value.equals("AES/GCM/PKCS5Padding"))
+			InvokeExpr invExpr = st.getInvokeExpr(); 
+			SootMethod invMeth = invExpr.getMethod(); 
+			SootClass invDeclClass = invExpr.getMethod().getDeclaringClass(); 
+			String invName = invMeth.getName();
+			String invDeclName = invDeclClass.getName();
+			if (invName.equals(invMethodName) && invDeclName.equals(invClassName)) {  // check if the invoke expression is a call to Cipher.getInstance
+				if (invExpr.getArg(0) instanceof StringConstant) { // check if the argument is a string constant
+					StringConstant constant = (StringConstant) invExpr.getArg(0); 
+					if (!constant.value.equals(correctConvention)) {
 						reporter.reportVulnerability(method.getSignature(), unit);
+					}
 				}
 			}
 		}
