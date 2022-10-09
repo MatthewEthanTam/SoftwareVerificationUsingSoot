cb0d0756a74b59d217b55966be0feb5e802c1601
Matthew Tam <matthewethan.tam@gmail.com>
2022-10-09 15:56:45 +0200

    adding

--- Exercise1/intraprocedural_analysis_src/intraprocedural/src/main/java/analysis/exercise2/TypeStateAnalysis.java
+++ Exercise1/intraprocedural_analysis_src/intraprocedural/src/main/java/analysis/exercise2/TypeStateAnalysis.java
@@ -1,12 +1,12 @@
 package analysis.exercise2;
-
+import analysis.FileState;
 import java.util.Set;
-
+import java.util.HashSet;
 import analysis.FileStateFact;
 import analysis.ForwardAnalysis;
 import analysis.VulnerabilityReporter;
-import soot.Body;
-import soot.Unit;
+import soot.jimple.*;
+import soot.*;
 
 public class TypeStateAnalysis extends ForwardAnalysis<Set<FileStateFact>> {
 
@@ -17,24 +17,84 @@ public class TypeStateAnalysis extends ForwardAnalysis<Set<FileStateFact>> {
 	@Override
 	protected void flowThrough(Set<FileStateFact> in, Unit unit, Set<FileStateFact> out) {
 		copy(in, out);
-		// TODO: Implement your flow function here.
+		if (unit instanceof InvokeStmt) {
+			InvokeStmt invokeStmt = (InvokeStmt) unit;
+			InvokeExpr expr = invokeStmt.getInvokeExpr();
+			if (expr instanceof SpecialInvokeExpr) {
+				SpecialInvokeExpr specialInvoke = (SpecialInvokeExpr) expr;
+				Value base = specialInvoke.getBase();
+				SootMethod method = specialInvoke.getMethod();
+				if (method.getSignature().equals("<target.exercise2.File: void <init>()>")) {
+					Set<Value> aliases = new HashSet<Value>();
+					aliases.add(base);
+					FileStateFact fact= new FileStateFact(aliases, FileState.Init);
+					out.add(fact);
+				}
+			} else if (expr instanceof VirtualInvokeExpr) {
+				VirtualInvokeExpr virtualInvoke = (VirtualInvokeExpr) expr;
+				Value base = virtualInvoke.getBase();
+				SootMethod method = virtualInvoke.getMethod();
+				if (method.getSignature().equals("<target.exercise2.File: void open()>")) {
+					for (FileStateFact fact:out) {
+						if (fact.containsAlias(base)) {
+							if (fact.getState().equals(FileState.Init) || fact.getState().equals(FileState.Close))
+								fact.updateState(FileState.Open);
+						}
+					}
+				} else if (method.getSignature().equals("<target.exercise2.File: void close()>")) {
+					for (FileStateFact fact: out){
+						if (fact.containsAlias(base)) {
+							if (fact.getState().equals(FileState.Open))
+								fact.updateState(FileState.Close);
+						}
+					}
+				}
+
+			}
+		} else if (unit instanceof AssignStmt) {
+			AssignStmt assignStmt = (AssignStmt) unit;
+			Value leftOp = assignStmt.getLeftOp();
+			Value rightOp = assignStmt.getRightOp();
+			for (FileStateFact fact: out){
+				if (fact.containsAlias(rightOp)) {
+					fact.addAlias(leftOp);
+				}
+			}
+		} else if (unit instanceof ReturnVoidStmt) {
+			for (FileStateFact fact: out){
+				if (fact.isOpened()) {
+					reporter.reportVulnerability(this.method.getSignature(), (Stmt) unit);
+				}
+			}
+		}
 		prettyPrint(in, unit, out);
 	}
 
 	@Override
 	protected Set<FileStateFact> newInitialFlow() {
-		// TODO: Implement your initialization here.
-		return null;
+		return new HashSet<FileStateFact>();
 	}
 
 	@Override
 	protected void copy(Set<FileStateFact> source, Set<FileStateFact> dest) {
-		// TODO: Implement the copy function here.
+		for(FileStateFact f: source)
+		{
+			dest.add(f.copy());
+		}
 	}
 
 	@Override
 	protected void merge(Set<FileStateFact> in1, Set<FileStateFact> in2, Set<FileStateFact> out) {
-		// TODO: Implement the merge function here.
+		for(FileStateFact f: in1)
+		{
+			if(!out.contains(f))
+				out.add(f);
+		}
+		for(FileStateFact f: in2)
+		{
+			if(!out.contains(f))
+				out.add(f);
+		}
 	}
 
 }
