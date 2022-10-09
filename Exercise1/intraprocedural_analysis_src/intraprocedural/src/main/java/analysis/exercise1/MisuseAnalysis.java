d061d3f125a252949f2ae02b97d5b07c4f99c7a7
Matthew Tam <matthewethan.tam@gmail.com>
2022-10-09 15:30:43 +0200

    mis

--- Exercise1/intraprocedural_analysis_src/intraprocedural/src/main/java/analysis/exercise1/MisuseAnalysis.java
+++ Exercise1/intraprocedural_analysis_src/intraprocedural/src/main/java/analysis/exercise1/MisuseAnalysis.java
@@ -12,24 +12,48 @@ public class MisuseAnalysis extends AbstractAnalysis{
 	
 	@Override
 	protected void flowThrough(Unit unit) {
-		String invClassName = "javax.crypto.Cipher";
-		String invMethodName = "getInstance";
+		String cypherClass = "javax.crypto.Cipher";
+		String getInstanceCall = "getInstance";
 		String correctConvention = "AES/GCM/PKCS5Padding";
-		Stmt st = (Stmt) unit;
-		if (st.containsInvokeExpr()) { // check if the statement contains an invoke expression
-			InvokeExpr invExpr = st.getInvokeExpr(); 
-			SootMethod invMeth = invExpr.getMethod(); 
-			SootClass invDeclClass = invExpr.getMethod().getDeclaringClass(); 
-			String invName = invMeth.getName();
-			String invDeclName = invDeclClass.getName();
-			if (invName.equals(invMethodName) && invDeclName.equals(invClassName)) {  // check if the invoke expression is a call to Cipher.getInstance
-				if (invExpr.getArg(0) instanceof StringConstant) { // check if the argument is a string constant
-					StringConstant constant = (StringConstant) invExpr.getArg(0); 
-					if (!constant.value.equals(correctConvention)) {
-						reporter.reportVulnerability(method.getSignature(), unit);
-					}
+		/**
+		 *  Since Stmt and Unit are the same and
+		 *  we are only interested in the `Cipher.getInstance()`,
+		 *  we can cast the Unit to a Stmt because Stmt can get and
+		 *  see if an expression is created such as `Cipher.getInstance()`
+		 */
+		
+		Stmt st = (Stmt) unit; 		
+
+		/**
+		 *  We then want to check if the statements in the code contain that invokes
+		 *  an expression. such as the Cipher aesChipher; method.
+		 * 
+		 *  Thus if the statement does contain an expression we retrieve the expression, method and the class
+		 *  of the method and check if the class name is equal to `javax.crypto.Cipher` which is the cypher class 
+		 * 	and the method name is equal to `getInstance` which is the method we are looking for.
+		 * 
+		 * 	Then we check if the expressions first argument is a String but since it is 
+		 *  in Soot we check the StringConstant class using InstanceOf.
+		 * 
+		 *  Finally we check if the expression's first parameter is equal to `AES/GCM/PKCS5Padding`
+		 *  if not then it throws an error.
+		 * 
+		 *  */	
+		 
+		if (st.containsInvokeExpr()) {
+			InvokeExpr exprSoot = st.getInvokeExpr(); 
+			SootMethod methodSoot =  exprSoot.getMethod(); 
+			SootClass methodClassSoot = exprSoot.getMethod().getDeclaringClass(); 
+			String methodName = methodSoot.getName();
+			String methodClassName = methodClassSoot.getName();
+
+			if (methodName.equals(getInstanceCall) && // Check if the method name is equal to `getInstance`
+				methodClassName.equals(cypherClass) && // Check if the class name is equal to `javax.crypto.Cipher`
+				exprSoot.getArg(0) instanceof StringConstant &&  // Check if the first argument is a String
+				!((StringConstant) exprSoot.getArg(0)).value.equals(correctConvention)) // Check if the first parameter of expression is equal to `AES/GCM/PKCS5Padding`
+				{ 
+					reporter.reportVulnerability(method.getSignature(), unit);
 				}
 			}
 		}
 	}
-}
